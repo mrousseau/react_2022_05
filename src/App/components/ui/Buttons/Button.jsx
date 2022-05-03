75160025aedc1d272f722f4bc7569da1519b6e51
mathieu <mr.rousseaumathieu@gmail.com>
2022-05-03 15:15:02 +0200

    useState dans composent en function

--- src/App/components/ui/Buttons/Button.jsx
+++ src/App/components/ui/Buttons/Button.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'; 
 
 import style from './Button.module.css'
 
 const Button = (props) => {
-    console.log(props);
+    
+    const [isClicked, setIsClicked] = useState(false);
+
     return <button 
         className={style.Button} 
         style={{...props.style, backgroundColor: props.bgColor, color: props.color}}
         type={props.type}
         onClick={(evt)=>{
+            setIsClicked(true);
             props.onButtonClicked();
         }}
         
-        >{props.children}</button>
+        >{props.children}
+        <br/>
+        {isClicked.toString()}
+        </button>
 }
 Button.PropType={
     children: PropTypes.any.isRequired,
