3521f7f8656bd234861f8f66fcd150e9999404a8
mathieu <mr.rousseaumathieu@gmail.com>
2022-05-03 15:02:58 +0200

    rajout d'un state

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
 import Button from './components/ui/Buttons/Button'
 
-
-function App() {
-  return (
-    <div className="App">
-      <Button toto="un text du button" bgColor='red'></Button>
-      <Button color='orange'>du text children</Button>
-      <Button color='black' onButtonClicked= {()=>{
-        console.log('buttonClick');
-      }}>
-        <div>du text children node </div>
-      </Button>
-      <Button bgColor='blue' style={{textDecoration: 'underline', fontWeight:900, fontStyle: 'Arial'}}>
-        <div>du text children node 1</div>
-        <div>du text children node 2</div>
-      </Button>
-    </div>
-  );
+class App extends React.Component {
+constructor(props) {
+  super(props)
+  this.state={count:1, title:"title de l'app"}
 }
-export default App;
+  
+
+  render() {
+    return (
+      <div className='App'>
+        Valeur de count : {this.state.count}
+        <hr/>
+        <Button onButtonClicked={()=>{
+          this.setState({count:this.state.count+1})
+          this.count++;
+          console.log(this.state.count);
+        }} bgColor='blue'>ajouter +1</Button>
+        <Button onButtonClicked={()=>{
+          this.setState({count:this.state.count-1})
+          console.log(this.state.count);
+        }} bgColor='tomato'>retirer -1</Button>
+      </div>
+    )
+  }
+}
+
+export default App; 
