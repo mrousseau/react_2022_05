4e43eedbe38f81e4ff7191072b5e7079ce5fc18e
mathieu <mr.rousseaumathieu@gmail.com>
2022-05-05 10:02:20 +0200

    Add save MEME

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -12,6 +12,7 @@ const ressourcesInitialState = {
 export const ACTION_RESSOURCES = Object.freeze({
   ADD_ALL_RESSOURCES: "ADD_ALL_RESSOURCES",
   INIT_RESSOURCES: "INIT_RESSOURCES",
+  ADD_SAVE_MEME: "ADD_SAVE_MEME",
   ADD_MEME: "ADD_MEME",
 });
 
@@ -40,12 +41,28 @@ function ressourcesReducer(state = ressourcesInitialState, action) {
 
   export const ACTION_CURRENT = Object.freeze({
     UPDATE_MEME: "UPDATE_MEME",
+    CLEAR_CURRENT:"CLEAR_CURRENT",
+    SAVE_MEME:"SAVE_MEME"
   });
 
   function currentReducer(state = DummyMeme, action) {
     switch (action.type) { 
       case ACTION_CURRENT.UPDATE_MEME:
         return { ...state, ...action.value };
+      case ACTION_CURRENT.CLEAR_CURRENT:
+      case ACTION_RESSOURCES.ADD_SAVE_MEME:
+        return { ...DummyMeme }; 
+      case ACTION_CURRENT.SAVE_MEME:
+        fetch(`${ADDR_REST} + /memes${undefined!==state.id? '/' + state.id: ""}`,
+        {
+          method: `${undefined !== state.id ? "PUT" : "POST"}`,
+          headers: { "content-Type": "application/json" },
+          body: JSON.stringify(state),
+        }
+        ).then((flux) => flux.json()
+        ).then(o=>{
+          store.dispatch({type: ACTION_RESSOURCES.ADD_SAVE_MEME, value: o })
+        });
       default:
         return state;
     }
@@ -55,59 +72,3 @@ function ressourcesReducer(state = ressourcesInitialState, action) {
   export const store = createStore(combinedReduc,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
   store.dispatch({type: ACTION_RESSOURCES.INIT_RESSOURCES});
-
-
-
-
-// import {createStore} from 'redux'
-// import { ADDR_REST } from '../config/config';
-
-// const ressourcesInitialState = {
-//     images:[],
-//     memes: [],
-// }
-
-// const ACTION_RESSOURCES = Object.freeze({
-//     ADD_ALL_RESSOURCES:'ADD_ALL_RESSOURCES',
-//     INIT_RESSOURCES:'INT_RESSOURCES',
-//     ADD_MEME:'ADD_MEME'
-// })
-
-// /**
-//  * reducer pour les ressources
-//  * @param {object} state 
-//  * @param {object} action 
-//  */
-// function ressourcesReducer(state=ressourcesInitialState, action) {
-//     switch(action.type){
-//         case ACTION_RESSOURCES.ADD_IMAGE : return {...state, memes:[...state.memes, action.value]};
-//         case ACTION_RESSOURCES.INIT_RESSOURCES: 
-//         const memes = fetch(ADDR_REST+"/memes").then(flux=>flux.json());
-//         const images = fetch(ADDR_REST+"/images").then(flux=>flux.json());
-//         Promise.all([memes, images]).then(arr=>{
-//             store.dispatch({type:ACTION_RESSOURCES.ADD_ALL_RESSOURCES, values:arr})});
-//         return state;
-
-//         case ACTION_RESSOURCES.ADD_ALL_RESSOURCES: return {...state, memes:action.value[0],images:action.value[1]}
-//         default: return state;
-//     }
-// }
-
-// export const store=createStore(ressourcesReducer);
-// store.dispatch({type:ACTION_RESSOURCES.INIT_RESSOURCES});
-// // store.subscribe(()=>{
-// //     console.log(store.getState())
-// // })
-// // store.dispatch({type:ACTION_RESSOURCES.ADD_MEME, value:{id:2, text:'nouveau meme'}})
-// // store.dispatch({type:ACTION_RESSOURCES.ADD_MEME, value:{id:3, text:'noue'}})
-// // store.dispatch({type:ACTION_RESSOURCES.ADD_MEME, value:{id:4, text:'titi'}})
-// // store.dispatch({type:ACTION_RESSOURCES.ADD_MEME, value:{id:5, text:'toto'}})
-// // store.dispatch({type:ACTION_RESSOURCES.ADD_MEME, value:{id:6, text:'meme'}})
-// /*
-// let state=ressourcesReducer(undefined, {type:'INIT'});
-// console.log(state)
-// state=ressourcesReducer(state, {type:ACTION_RESSOURCES.ADD_MEME, value:{id:2, text:'nouveau meme'}});
-// console.log(state)
-// state=ressourcesReducer(state, {type:ACTION_RESSOURCES.ADD_MEME, value:{id:1, text:'nouveau meme'}});
-// console.log(state)
-// */
