f1f2702f84708c732b7b0432cfa650ba546ec897
mathieu <mr.rousseaumathieu@gmail.com>
2022-05-05 09:38:14 +0200

    cleaned code

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,66 +1,22 @@
 // import { MemeSVGViewer } from 'orsys-tjs-meme';
-import { DummyMeme, IImage, IMeme } from '../interfaces/common';
 import React from 'react'
 import FlexW from './components/layout/FlexW/FlexW';
-import MemeForm, { ConnectedMemeForm } from './components/ui/MemeForm/MemeForm';
-// import Button from './components/ui/Buttons/Button'
-import { ADDR_REST } from './config/config';
-import MemeThumbnail, { ConnectedMemThumbnail } from '../App/components/ui/MemeThunmbnail/MemeThunmbnail';
-import {store} from './store/store';
+import { ConnectedMemeForm } from './components/ui/MemeForm/MemeForm';
+import { ConnectedMemThumbnail } from '../App/components/ui/MemeThunmbnail/MemeThunmbnail';
 import MemeSVGViewer from './components/ui/connectedMemeViewer/connectedMemeViewer'
 // './components/ui/ConnectedMemeViewer/ConnectedMemeViewer'
 
-interface IAppSate{
-  memes:Array<IMeme>,
-  images:Array<IImage>,
-  current:IMeme
-}
-
-interface IappProps{}
-
-class App extends React.Component<IappProps,IAppSate> {
-  constructor(props) {
-    super(props);
-    this.state = { memes:[], images: [], current:DummyMeme}
-  }
-
-  componentDidMount(){
-    const memes = fetch(ADDR_REST+"/memes").then(flux=>flux.json());
-    const images = fetch(ADDR_REST+"/images").then(flux=>flux.json());
-    Promise.all([memes, images]).then(arr=>{
-      this.setState({memes:arr[0], images:arr[1]})});
-  }
-
-  componentDidUpdate(oldProps, oldState)
-  {
-    console.log('component update ',oldState, this.state );
-  }
-  //attend que tout soit chargé
-
-
-  render() {
+const App=() => {
     return (
       <div className='App'>
-      {/* <Button></Button> */}
-      {/* <MemeThumbnail memes={this.state.memes} images={this.state.images}/> */}
-      <ConnectedMemThumbnail />
-      <FlexW>
-      {/* {this.state.memes.length && <MemeSVGViewer meme={this.state.current} image={this.state.images.find(e=> e.id === this.state.current.imageId)} />
-      } */}
-      <MemeSVGViewer />
-      {/* <MemeForm 
-      images={this.state.images} 
-      meme={this.state.current} 
-      onFormChange={(objt: object) =>{
-        this.setState({current: {...this.state.current, ...objt}})
-      }} 
-      /> */
-      <ConnectedMemeForm />}
-      </FlexW>
-        {/* {JSON.stringify(this.state)} */}
+        <ConnectedMemThumbnail />
+        <FlexW>
+        <MemeSVGViewer />
+        <ConnectedMemeForm />
+        </FlexW>
       </div>
     )
-  }
 }
+
 
 export default App; 
